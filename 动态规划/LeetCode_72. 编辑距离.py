b865238bbb7b33e5d20a1ed683ccbc048becd50b
cool <cool_kj@126.com>
2019-12-18 23:16:33 +0800

    72. 编辑距离

--- 动态规划/LeetCode_72. 编辑距离.py
+++ 动态规划/LeetCode_72. 编辑距离.py
@@ -106,5 +106,6 @@ class Solution3(object):
                 if word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+                    dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+
         return dp[m][n]
